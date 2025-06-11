b38b248c8e7397f1b69c91d810e52abc9aca9542
Thanawat Techakasemsuk <iamthanawat.t@gmail.com>
2025-06-12 05:05:16 +0700

    Fetching data

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,11 +1,13 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import BaseContainer from '../components/Container';
 import Button from '../components/Button';
 import Input from '../components/Input';
 
+import { useParams } from 'react-router-dom';
+
 import { numberWithCommas } from '../utils';
-import { products } from '../data';
 
 const Container = styled(BaseContainer)`
   padding-top: 78px;
@@ -54,25 +56,38 @@ const Description = styled.p`
   margin-bottom: 72px;
 `;
 
-const data = products[0];
-
 /**
  * Below is the main ProductDetail component.
  */
-export const ProductDetail = () => (
-  <Container>
-    <ProductImage src={data.imageUrl} alt={`${data.name}`} />
-    <ProductInfo>
-      <Subtitle>
-        <span>{data.category}</span>
-        <span>฿{numberWithCommas(data.price)}</span>
-      </Subtitle>
-      <Title>{data.name}</Title>
-      <Description>{data.description}</Description>
-      <Input style={{ marginBottom: '40px' }} type={'number'} label={'Quantity'} />
-      <Button>Add to Cart</Button>
-    </ProductInfo>
-  </Container>
-);
+export const ProductDetail = () => {
+  const { productId } = useParams();
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(false);
+    fetch('https://us-central1-skooldio-react-hooks.cloudfunctions.net/products/' + productId)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProduct(data);
+        setLoading(true);
+      });
+  });
+  if (loading || !product) return <div>Loading...</div>;
+  return (
+    <Container>
+      <ProductImage src={product.imageUrl} alt={`${product.name}`} />
+      <ProductInfo>
+        <Subtitle>
+          <span>{product.category}</span>
+          <span>฿{numberWithCommas(product.price)}</span>
+        </Subtitle>
+        <Title>{product.name}</Title>
+        <Description>{product.description}</Description>
+        <Input style={{ marginBottom: '40px' }} type={'number'} label={'Quantity'} />
+        <Button>Add to Cart</Button>
+      </ProductInfo>
+    </Container>
+  );
+};
 
 export default ProductDetail;
